7b38a69e2a949620c39b9cb35a4aa6f975eac61f
Phoenix Campos <pacampos@usc.edu>
2016-09-07 16:10:37 -0700

    test comment

--- NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/MainActivity.java
+++ NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+//    Test Comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

2307b8ad497e0d749ceb89d7e0748941ed52d989
Vishnu Venkateswaran <anvenkat@usc.edu>
2016-09-07 19:06:45 -0700

    basic changes

--- NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/MainActivity.java
+++ NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.nutrition.nutritionapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.Color;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get references to elements on page
+        Button signupButton = (Button) this.findViewById(R.id.signupButton);
+        Button loginButton = (Button) this.findViewById(R.id.loginButton);
+        signupButton.setBackgroundColor(Color.parseColor("#5167bd"));
+        loginButton.setBackgroundColor(Color.parseColor("#5167bd"));
     }
+
+
 }

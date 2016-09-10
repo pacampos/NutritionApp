2aa12eb7bca6ade1afb3e68b8801c7b11db33bb1
Vishnu Venkateswaran <anvenkat@usc.edu>
2016-09-10 12:16:01 -0700

    Sign up page initial layout. Will change editText font and separate goals and measurements into different pages

--- NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/SignUpActivity.java
+++ NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.example.nutrition.nutritionapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // Get references
+
+        Button nextButton = (Button) this.findViewById(R.id.nextButton);
+        nextButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
     }
 }

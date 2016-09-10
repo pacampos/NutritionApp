ee87698430d76cdfde7a5d5790c1273d4db6f4fb
Vishnu Venkateswaran <anvenkat@usc.edu>
2016-09-09 20:24:46 -0700

    Initial updates to welcome and sign up screens

--- NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/MainActivity.java
+++ NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.nutrition.nutritionapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.graphics.Color;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +18,17 @@ public class MainActivity extends AppCompatActivity {
         // Get references to elements on page
         Button signupButton = (Button) this.findViewById(R.id.signupButton);
         Button loginButton = (Button) this.findViewById(R.id.loginButton);
-        signupButton.setBackgroundColor(Color.parseColor("#5167bd"));
-        loginButton.setBackgroundColor(Color.parseColor("#5167bd"));
+        signupButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        loginButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+        // Go to new activity when clicking sign up button
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 

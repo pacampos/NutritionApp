af9de4cac0fe11406262406347bd32e165ba6843
Phoenix Campos <pacampos@usc.edu>
2016-09-12 22:32:33 -0700

    added storage and authentication for usernames/passowords

--- NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/NutritionSingleton.java
+++ NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/NutritionSingleton.java
@@ -1,10 +1,16 @@
 package com.example.nutrition.nutritionapp;
 
+import com.example.nutrition.nutritionapp.Model.AccountModel;
+
+import java.util.List;
+
 /**
  * Created by phoenixcampos01 on 9/12/16.
  */
 public class NutritionSingleton {
     private static NutritionSingleton mInstance = null;
+
+    private List<AccountModel> accounts;
 
     private NutritionSingleton(){
     }

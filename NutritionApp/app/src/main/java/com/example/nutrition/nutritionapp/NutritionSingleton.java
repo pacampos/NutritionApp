f4a88448713dbc56a21292cc968c04c03317f203
Phoenix Campos <pacampos@usc.edu>
2016-09-14 10:10:56 -0700

    beginning the database singleton - possibly unnecessary, but a good place to put it for now

--- NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/NutritionSingleton.java
+++ NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/NutritionSingleton.java
@@ -1,6 +1,9 @@
 package com.example.nutrition.nutritionapp;
 
 import com.example.nutrition.nutritionapp.Model.AccountModel;
+import com.firebase.client.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
@@ -8,11 +11,14 @@ import java.util.List;
  * Created by phoenixcampos01 on 9/12/16.
  */
 public class NutritionSingleton {
+//    private FirebaseDatabase databaseInstance;
     private static NutritionSingleton mInstance = null;
 
     private List<AccountModel> accounts;
 
     private NutritionSingleton(){
+        DatabaseReference databaseInstance=FirebaseDatabase.getInstance().getReference();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 
     public static NutritionSingleton getInstance(){

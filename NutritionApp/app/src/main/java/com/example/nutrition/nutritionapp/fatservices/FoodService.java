6d8aec608686b5c1289882478123670f4d69de0c
Jimmy Hsu <jimmyhsu@usc.edu>
2016-10-04 22:43:20 -0700

    API INFO

---
+++ NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/fatservices/FoodService.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2016 Saurabh Rane
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.nutrition.nutritionapp.fatservices;
+
+import android.util.Log;
+
+import com.example.nutrition.nutritionapp.Model.CompactFood;
+import com.example.nutrition.nutritionapp.Model.Food;
+import com.example.nutrition.nutritionapp.utils.FoodUtility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This service class helps to get or search food items from fatsecret rest api
+ *
+ * @author Saurabh Rane
+ * @version 1.0
+ */
+public class FoodService {
+	/** Request Object */
+	private Request request;
+	
+	/**
+	 * Constructor to set values for APP_KEY and APP_SECRET
+	 *
+	 * @param APP_KEY		a value FatSecret API issues to you which helps this API identify you
+	 * @param APP_SECRET	a secret FatSecret API issues to you which helps this API establish that it really is you
+	 */
+	public FoodService(String APP_KEY, String APP_SECRET) {
+		request = new Request(APP_KEY, APP_SECRET);
+	}
+	
+	/**
+	 * Returns detailed nutritional information for the specified food
+	 *
+	 * @param foodId		the unique food identifier
+	 * @return				food based on the identifier
+	 */
+	public Food getFood(Long foodId) {
+		JSONObject response = request.getFood(foodId);
+		if(response != null) {
+			JSONObject food = null;
+			try {
+				food = response.getJSONObject("food");
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			return FoodUtility.parseFoodFromJSONObject(food);
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns response associated with the food items at zeroth page depending on the search query
+	 * 
+	 * @param query			search terms for querying food items
+	 * @return				food items at zeroth page based on the query
+	 */
+	public Response<CompactFood> searchFoods(String query) {
+		return searchFoods(query, 0);
+	}
+
+	/**
+	 * Returns response associated with the food items depending on the search query and page number
+	 * 
+	 * @param query			search terms for querying food items
+	 * @param pageNumber	page Number to search the food items
+	 * @return				food items at a particular page number based on the query
+	 */
+	public Response<CompactFood> searchFoods(String query, Integer pageNumber) {
+		JSONObject json = request.getFoods(query, pageNumber);
+		
+		if(json != null) {
+			JSONObject foods = null;
+			try {
+				foods = json.getJSONObject("foods");
+			} catch (JSONException e) {
+                Log.d("FppdService", "exception looking for food");
+                e.printStackTrace();
+			}
+
+			int maxResults = 0;
+			try {
+				maxResults = foods.getInt("max_results");
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			int totalResults = 0;
+			try {
+				totalResults = foods.getInt("total_results");
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+			List<CompactFood> results = new ArrayList<CompactFood>();
+			
+			if(totalResults > maxResults * pageNumber) {
+				JSONArray food = null;
+				try {
+					food = foods.getJSONArray("food");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				results = FoodUtility.parseCompactFoodListFromJSONArray(food);
+			}
+			
+			Response<CompactFood> response = new Response<CompactFood>();
+			
+			response.setPageNumber(pageNumber);
+			response.setMaxResults(maxResults);
+			response.setTotalResults(totalResults);
+			response.setResults(results);
+			
+			return response;
+		}		
+		return null;
+	}	
+}
--- NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/foodEntryFragment.java
+++ NutritionApp/app/src/main/java/com/example/nutrition/nutritionapp/foodEntryFragment.java
@@ -1,20 +1,99 @@
 package com.example.nutrition.nutritionapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.TextViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import com.example.nutrition.nutritionapp.Model.CompactFood;
+import com.example.nutrition.nutritionapp.fatservices.FoodService;
+import com.example.nutrition.nutritionapp.fatservices.Response;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 public class foodEntryFragment extends Fragment {
     public foodEntryFragment() {
         // Required empty public constructor
     }
+    TextView textView;
+    String API_SECRET= "118c4828b97848de9d1576137f9541b1";
+    String API_CONSUMER = "739bfa1b0dd3407882ac1b24c5be4167";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_entry, container, false);
+        View v= inflater.inflate(R.layout.fragment_food_entry, container, false);
+        Button foodEntryButton = (Button) v.findViewById(R.id.button_log_food);
+        final android.widget.SearchView searchView = (SearchView) v.findViewById(R.id.search_bar_food);
+        textView= (TextView) v.findViewById(R.id.textview_serving_size);
+
+        foodEntryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query=searchView.getQuery().toString();
+                if(!query.isEmpty()){
+
+                    new MyTask().execute(query);
+
+
+                }
+            }
+        });
+
+        return v;
+    }
+
+
+    private class MyTask extends AsyncTask<String, Integer, String> {
+
+        List<CompactFood> listFoods;
+        // Runs in UI before background thread is called
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            // Do something like display a progress bar
+        }
+
+
+        // This is run in a background thread
+        @Override
+        protected String doInBackground(String... params) {
+            String query=params[0];
+            FoodService userFood= new FoodService(API_CONSUMER,API_SECRET);
+            Response<CompactFood> foods=userFood.searchFoods(query);
+            if(foods != null){
+              listFoods=foods.getResults();
+                return "results";
+            }
+
+            return null;
+        }
+
+        // This is called from background thread but runs in UI
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+            // Do things like update the progress bar
+        }
+
+        // This runs in UI when background thread finishes
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute("result");
+            textView.setText( listFoods.get(0).getName());
+
+            // Do things like hide the progress bar or change a TextView
+        }
     }
 }
